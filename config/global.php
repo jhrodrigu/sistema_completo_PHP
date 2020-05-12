927fb7ed3ad4da7d78d6d7777221af4c6691af79
Yonny Rodriguez <jhrodrigu@hotmail.com>
2020-05-12 02:31:50 -0500

    Creacion de un sistema

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+
+// Ip de la pc seervidor de base de dato
+define("DB_HOST","localhost");
+
+// Nombre de la base de datos
+define("DB_NAME","dbsistema");
+
+// Usuario de la base de datos
+define("DB_USERNAME","root");
+
+// Contraseña del usuario de la base de datos
+define("DB_PASSWORD","");
+
+// definimos la decodificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+// Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
